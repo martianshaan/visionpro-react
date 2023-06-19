6d4565fa8be3cfb0f5d7d34c5bc9f0ff445338bb
martianshaan <shantanu.kale@viit.ac.in>
2023-06-19 12:29:55 +0530

    changes to vite eslint plugin

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,10 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import eslintPlugin from 'vite-plugin-eslint'
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    eslintPlugin({
-      cache: false,
-      include: ['./src/**/*.js', './src/**/*.jsx'],
-      exclude: [],
-    }),
+    react()
   ],
 });
