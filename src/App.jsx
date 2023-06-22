1307976ea3966cf5c37a032f3135c9f68937eba9
martianshaan <shantanu.kale@viit.ac.in>
2023-06-23 01:01:44 +0530

    Add:signup page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './custom.styles.css';
 import { React } from 'react';
 import Navbar from './components/navbar/Navbar';
 import Home from './pages/Home';
+import Signup from './pages/Signup';
 
 function App() {
   return (
     <div className="flex-row">
       <Navbar />
       <Home/>
+      <Signup />
     </div>
   );
 }
