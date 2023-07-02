d52b9877aec637e189091d4997bd127f4118149c
martianshaan <shantanu.kale@viit.ac.in>
2023-07-02 20:19:36 +0530

    Update:Logo style

--- src/components/navbar/Logo.jsx
+++ src/components/navbar/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Logo() {
   return (
-    <div className="font-monoton text-3xl hover:text-orange-600 cursor">
+    <div className="font-monoton text-3xl hover:text-orange-600 cursor-pointer text-center">
       SUNBUN
     </div>
   );
