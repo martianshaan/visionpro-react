28221caceebe95318dce94d9167abb57578b8680
martianshaan <shantanu.kale@viit.ac.in>
2023-07-04 11:04:15 +0530

    Update: already an account

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import bannerHero from '../assets/bannerHero.jpg';
+import { Logo } from '../components';
 
 function Signup() {
   return (
@@ -10,9 +11,7 @@ function Signup() {
       </figure>
       <div className="flex items-center justify-center w-full px-5 mt-3">
         <section className="px-7 py-10 rounded-md shadow-md bg-white/[0.75] flex flex-col gap-6 w-full max-w-lg">
-          <h6 className="font-monoton text-3xl hover:text-red-400 cursor-pointer text-center">
-            sunbun
-          </h6>
+          <Logo />
           <section className="flex flex-col gap-3">
             <h1 className="text-4xl font-bold text-center">Sign up</h1>
             <form className="flex flex-col gap-3">
@@ -31,9 +30,17 @@ function Signup() {
             </form>
           </section>
           <section className="w-full py-2   flex flex-col gap-4 items-center ">
-            <button type="submit" className="rounded-md btn-primary w-2/3 text-lg text-center">
-              Create
+            <button type="submit" className="rounded-md btn-primary w-2/3 text-lg font-bold text-center">
+              Create Account
             </button>
+            <p className="text-gray-700 text-md flex gap-1">
+              Already have an account ?
+              {' '}
+              {' '}
+              <span className="underline text-base ml-1">
+                Login
+              </span>
+            </p>
           </section>
 
         </section>
