7c9cf2136a718f59f305fc25613a7608ec5944b4
martianshaan <shantanu.kale@viit.ac.in>
2023-07-06 14:08:59 +0530

    Update: add checkout page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import './custom.styles.css';
 import { React } from 'react';
 import Navbar from './components/navbar/Navbar';
-import Home from './pages/Home';
-import ProductListing from './pages/ProductListing';
-import Signup from './pages/Signup';
-import ProductData from './pages/ProductData';
-import Login from './pages/Login';
+// import Home from './pages/Home';
+// import ProductListing from './pages/ProductListing';
+// import Signup from './pages/Signup';
+// import ProductData from './pages/ProductData';
+// import Login from './pages/Login';
+import Checkout from './pages/Checkout';
 
 function App() {
   return (
     <div className="flex-row">
       <Navbar />
-      <Home />
+      {/* <Home />
       <ProductListing />
       <Signup />
       <ProductData />
-      <Login />
+      <Login /> */}
+      <Checkout />
     </div>
   );
 }
