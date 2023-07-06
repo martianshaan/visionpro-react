aae3c06c110daaa91a730032d0aa8ca55b485951
martianshaan <shantanu.kale@viit.ac.in>
2023-07-06 14:07:25 +0530

    Add:index file in pages

---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Home } from './Home';
+export { default as ProductListing } from './ProductListing';
+export { default as Signup } from './Signup';
+export { default as ProductData } from './ProductData';
+export { default as Checkout } from './Checkout';
