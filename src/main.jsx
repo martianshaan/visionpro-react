44ecbb94f1e70fec91dec1228ac7b7484cef0de8
martianshaan <shantanu.kale@viit.ac.in>
2023-07-16 21:07:02 +0530

    Update: wrap App with Browser Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-/* eslint-disable import/extensions */
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+
   </React.StrictMode>,
 );
