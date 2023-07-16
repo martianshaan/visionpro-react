1389af4e24b3f162d19d8d0b6c8f9ac072bf213c
martianshaan <shantanu.kale@viit.ac.in>
2023-07-16 21:17:03 +0530

    Update: ad Index page in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,11 @@
 import './custom.styles.css';
 import { React } from 'react';
-import Navbar from './components/navbar/Navbar';
-import Profile from './pages/Profile';
-// import Home from './pages/Home';
-// import ProductListing from './pages/ProductListing';
-import Signup from './pages/Signup';
-// import ProductData from './pages/ProductData';
-// import Login from './pages/Login';
-// import Checkout from './pages/Checkout';
+import Index from './routes/Index';
 
 function App() {
   return (
     <div className="flex-row">
-      <Navbar />
-      {/* <Home />
-      <ProductListing />
-      <Signup />
-      <ProductData />
-      <Login /> */}
-      {/* <Checkout /> */}
-      <Profile />
-      <Signup />
+      <Index />
     </div>
   );
 }
