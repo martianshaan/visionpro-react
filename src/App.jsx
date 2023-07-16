2cf5e1e7c505b2cc1a363ff88cb6588c4c68495e
martianshaan <shantanu.kale@viit.ac.in>
2023-07-16 22:56:41 +0530

    Update:comments removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,3 @@
-// import { React } from 'react';
-// import Index from './routes/Index';
-// import './custom.styles.css';
-
-// function App() {
-//   return (
-//     <div className="flex-row">
-//       <Index />
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
 import { Index as Route } from './routes/index';
 import './custom.styles.css';
