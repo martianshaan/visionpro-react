7e5de7a09f6fdd502be5716ec7a19e70312684be
martianshaan <shantanu.kale@viit.ac.in>
2023-07-17 09:48:11 +0530

    Removed : react.strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,10 @@ import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+  // <React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
 
-  </React.StrictMode>,
+  // </React.StrictMode>,
 );
