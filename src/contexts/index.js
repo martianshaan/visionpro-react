f3093d713b23bf56306dc0a116970826f85620f2
martianshaan <shantanu.kale@viit.ac.in>
2023-07-18 17:29:31 +0530

    ADD: Product context and auth context

--- src/contexts/index.js
+++ src/contexts/index.js
@@ -1,3 +1,10 @@
+import { useContext } from 'react';
+import { AuthContext } from './authContext/AuthContext';
+import { ProductContext } from './productContext/ProductContext';
+
 export { default as AuthContextProvider } from './authContext/AuthContext';
 
 export { default as ProductContextProvider } from './productContext/ProductContext';
+
+export const useAuthContext = () => useContext(AuthContext);
+export const useProductContext = () => useContext(ProductContext);
