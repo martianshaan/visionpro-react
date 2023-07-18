3f72ce568425be85ab049124caae1da78f79d104
martianshaan <shantanu.kale@viit.ac.in>
2023-07-18 17:21:39 +0530

    Add:local storage method to authcontext

--- src/contexts/authContext/AuthContext.jsx
+++ src/contexts/authContext/AuthContext.jsx
@@ -1,12 +1,32 @@
+/* eslint-disable react/jsx-no-constructed-context-values */
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable react/prop-types */
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useState } from 'react';
+import { useNavigate } from 'react-router';
 
 const AuthContext = createContext();
 
 function AuthContextProvider({ children }) {
+  const navigate = useNavigate();
+  const [isAutheneticated, setIsAutheneticated] = useState(
+    localStorage.getItem('authenticated'),
+  );
+
+  const [logginIn, setLoggingIn] = useState(false);
+
+  const Login = ({ email, password }) => {
+    setLoggingIn(true);
+    if (email && password) {
+      localStorage.setItem('authenticated', true);
+      setIsAutheneticated(true);
+      setTimeout(() => {
+        setLoggingIn(false);
+        navigate('/');
+      }, 1000);
+    }
+  };
   return (
-    <AuthContext.Provider>
+    <AuthContext.Provider value={{ isAutheneticated, logginIn, Login }}>
       {children}
     </AuthContext.Provider>
   );
