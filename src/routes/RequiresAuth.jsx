1a52023def376f943ef4daef3418b496d28b84a2
martianshaan <shantanu.kale@viit.ac.in>
2023-07-21 13:46:36 +0530

    Upate: isAuthenticated from context

--- src/routes/RequiresAuth.jsx
+++ src/routes/RequiresAuth.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
+import { useAuthContext } from '../contexts';
 
 function RequiresAuth() {
-  const isAuthenticated = true;
+  const { isAuthenticated } = useAuthContext();
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 }
 
