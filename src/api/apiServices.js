e1e59f66565d1bf2cc84fd25fdd1f0c06c38e86e
martianshaan <shantanu.kale@viit.ac.in>
2023-08-05 16:41:08 +0530

    Update: GET_ALL_PRODUCTS_URL

--- src/api/apiServices.js
+++ src/api/apiServices.js
@@ -1,8 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable import/prefer-default-export */
 import axios from 'axios';
-import { LOGIN_URL } from './apiUrls';
+import { GET_ALL_PRODUCTS_URL, LOGIN_URL } from './apiUrls';
 
 export const loginService = (email, password) => {
   axios.post(LOGIN_URL, { email, password });
 };
+
+export const getAllProductsUrl = () => axios.get(GET_ALL_PRODUCTS_URL);
