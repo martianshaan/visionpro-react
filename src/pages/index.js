7dd85039ff0dfe24dac5283e3f4798e2aa87504f
martianshaan <shantanu.kale@viit.ac.in>
2023-08-05 16:35:11 +0530

    Update: name change

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 export { default as Home } from './Home';
 export { default as ProductListing } from './ProductListing';
 export { default as Signup } from './Signup';
-export { default as ProductData } from './ProductData';
+export { default as ProductData } from './ProductDetails';
 export { default as Checkout } from './Checkout';
 export { default as Profile } from './Profile';
