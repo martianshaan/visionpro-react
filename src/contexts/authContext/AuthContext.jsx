5f6bc5fab23ad75861888a79b7c6ed63f58166dc
martianshaan <shantanu.kale@viit.ac.in>
2023-08-18 15:56:20 +0530

    add: Use Memo for auth values

--- src/contexts/authContext/AuthContext.jsx
+++ src/contexts/authContext/AuthContext.jsx
@@ -1,13 +1,9 @@
-/* eslint-disable import/prefer-default-export */
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/react-in-jsx-scope */
-/* eslint-disable max-len */
-/* eslint-disable no-alert */
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable react/prop-types */
-/* eslint-disable no-console */
-/* eslint-disable react/jsx-no-constructed-context-values */
-
-import { createContext, useState, useEffect } from 'react';
+import React, {
+  createContext, useState, useEffect, useMemo,
+} from 'react';
+import { toast } from 'react-hot-toast';
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
@@ -19,34 +15,28 @@ import {
 } from 'firebase/auth';
 import { auth } from '../../firebase';
 
-import { notify } from '../../utils/utils';
-
 export const AuthContext = createContext();
 
 function AuthContextProvider({ children }) {
   const [user, setUser] = useState({});
 
   const [signingUp, setSigningUp] = useState(false);
-  const [loggingIn, setLoggingIn] = useState(false);
 
   async function signupHandler(email, password, userName) {
-    console.log('username', userName);
     setSigningUp(true);
     try {
       // Create the user with email and password
       const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-      notify('success', 'Signup successful! You can now log in.');
       // Update the user's display name
       await updateProfile(userCredential.user, {
         displayName: userName,
       });
       // Refresh the user object with updated information
       setUser(userCredential.user);
-
       return userCredential;
     } catch (error) {
       // Handle errors
-      console.error(error.message);
+      toast.error(error.message);
       throw error;
     } finally {
       setSigningUp(false);
@@ -54,12 +44,10 @@ function AuthContextProvider({ children }) {
   }
 
   function loginHandler(email, password) {
-    notify('success', 'Logged in Successfully!!');
     return signInWithEmailAndPassword(auth, email, password);
   }
 
   function logoutHandler() {
-    notify('success', 'Logged out Successfully!!');
     return signOut(auth);
   }
   function googleSignIn() {
@@ -68,7 +56,6 @@ function AuthContextProvider({ children }) {
   }
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentuser) => {
-      console.log('Auth', currentuser);
       setUser(currentuser);
     });
 
@@ -76,16 +63,19 @@ function AuthContextProvider({ children }) {
       unsubscribe();
     };
   }, []);
+
+  const authValues = useMemo(() => ({
+    signupHandler,
+    loginHandler,
+    user,
+    logoutHandler,
+    googleSignIn,
+    signingUp,
+  }), [signingUp, user]);
+
   return (
     <AuthContext.Provider
-      value={{
-        signupHandler,
-        loginHandler,
-        user,
-        logoutHandler,
-        googleSignIn,
-        signingUp,
-      }}
+      value={authValues}
     >
       {children}
     </AuthContext.Provider>
