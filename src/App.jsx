d09f1aad5a672a9dc2a956cae548dabddd55b269
martianshaan <shantanu.kale@viit.ac.in>
2023-08-18 10:13:46 +0530

    Add: Toaster at top  of componentt tree

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
+import { Toaster } from 'react-hot-toast';
 import { Index as Route } from './routes/index';
 import './custom.styles.css';
 
@@ -6,6 +8,13 @@ function App() {
   return (
     <React.StrictMode>
       <main className="flex-row">
+        <Toaster
+          position="top-right"
+          reverseOrder={false}
+          containerStyle={{
+            top: '5rem',
+          }}
+        />
         <Route />
       </main>
     </React.StrictMode>
