00f80f35c0db73a8b878d9871c25b6a23057217a
martianshaan <shantanu.kale@viit.ac.in>
2023-08-22 19:35:39 +0530

    Create category context for categories

---
+++ src/contexts/categoriesContext/CategoriesContext.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable react/jsx-no-constructed-context-values */
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react/prop-types */
+import React, { createContext, useState, useEffect } from 'react';
+import { getCategoriesAndDocuments } from '../../firebase';
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const categoriesContext = createContext();
+
+function CategoriesContextProvider({ children }) {
+  const [categoriesMap, setCategoriesMap] = useState([]);
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      setCategoriesMap(categoryMap);
+    };
+    getCategoriesMap();
+  }, []);
+
+  return (
+    <categoriesContext.Provider value={{ categoriesMap }}>
+      {children}
+    </categoriesContext.Provider>
+  );
+}
+
+export default CategoriesContextProvider;
