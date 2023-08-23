f8b58af2188fd8edf40a2b73509c26dd13d0ac53
martianshaan <shantanu.kale@viit.ac.in>
2023-08-23 14:20:28 +0530

    Update: alert user to login

--- src/contexts/cartContext/CartContext.jsx
+++ src/contexts/cartContext/CartContext.jsx
@@ -1,21 +1,29 @@
+/* eslint-disable no-alert */
+/* eslint-disable import/no-cycle */
 /* eslint-disable react/jsx-no-constructed-context-values */
 /* eslint-disable react/prop-types */
 /* eslint-disable implicit-arrow-linebreak */
 import React, { createContext, useReducer } from 'react';
 import { cartReducer, INITIAL_STATE } from '../../reducers/cartReducer';
+import { useAuthContext } from '../index';
 
 export const CartContext = createContext();
 
 function CartContextProvider({ children }) {
   const [state, dispatch] = useReducer(cartReducer, INITIAL_STATE);
 
+  const { user } = useAuthContext();
   const addToCart = (id, image, name, newPrice, brand) => {
-    dispatch({
-      type: 'ADD_TO_CART',
-      payload: {
-        id, image, name, newPrice, brand,
-      },
-    });
+    if (user !== null) {
+      dispatch({
+        type: 'ADD_TO_CART',
+        payload: {
+          id, image, name, newPrice, brand,
+        },
+      });
+    } else {
+      alert('login first ');
+    }
   };
   return (
     <CartContext.Provider value={{ ...state, cart: state.cart, addToCart }}>
