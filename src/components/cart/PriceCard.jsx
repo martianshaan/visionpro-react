1466a7b185f69e80165c9b01c0462404d9327d7a
martianshaan <shantanu.kale@viit.ac.in>
2023-08-23 14:49:39 +0530

    ADD: cartitemcard , cartTotalcard and price card

---
+++ src/components/cart/PriceCard.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+function PriceCard({ product }) {
+  return (
+    <div key={product.id} className=" flex  justify-between  ">
+      <p className=" text-gray-600 flex-1">
+        {product.name}
+        {' '}
+        (
+        {product.qty}
+        )item
+      </p>
+      <p className="text-lg">
+        ₹
+        {product.price}
+      </p>
+    </div>
+  );
+}
+
+export default PriceCard;
