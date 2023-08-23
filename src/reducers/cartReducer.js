ba072d390d8a89348a877745211a075556daf04f
martianshaan <shantanu.kale@viit.ac.in>
2023-08-23 16:23:28 +0530

    ADD: check exisiting cart item , add qty, upd cart

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-case-declarations */
+/* eslint-disable max-len */
 /* eslint-disable no-console */
 /* eslint-disable import/prefer-default-export */
 
@@ -11,25 +13,29 @@ export const INITIAL_STATE = {
 export const cartReducer = (state, action) => {
   const { type, payload } = action;
   switch (type) {
-    case 'ADD_TO_CART': {
-      // const {
-      //   id, image, name, newPrice, brand,
-      // } = payload;
-      // // console.log(id, image, name, newPrice, brand);
-      // need when we have single product with diffrent colors or types
-      // const cartProduct = {
-      //   id,
-      //   name,
-      //   image,
-      //   newPrice,
-      //   brand,
-      // };
+    case 'ADD_TO_CART':
+      const existingCartItemIndex = state.cart.findIndex((item) => item.id === action.payload.id);
+
+      if (existingCartItemIndex !== -1) {
+        const updatedCart = state.cart.map((item, index) => (index === existingCartItemIndex
+          ? { ...item, qty: item.qty + 1 } // Increment quantity for the existing product
+          : item));
+
+        return {
+          ...state,
+          cart: updatedCart,
+        };
+      }
+      // If the item is not in the cart, add it with quantity
       return {
         ...state,
-        cart: [...state.cart, payload],
-      }; }
+        cart: [...state.cart, { ...payload, qty: 1 }],
+      };
 
     default:
-      throw new Error(`unhandled type of ${type} in cartReducer`);
+      return state;
   }
+
+  // default:
+  //   throw new Error(`unhandled type of ${type} in cartReducer`);
 };
