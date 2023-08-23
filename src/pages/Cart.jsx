7be771999cd8bee0674e15772c09f88e1544c798
martianshaan <shantanu.kale@viit.ac.in>
2023-08-23 17:20:15 +0530

    Add: cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,22 @@
+/* eslint-disable no-console */
 import React from 'react';
+import { useCartContext } from '../contexts';
+import { CartItemCard, CartTotalCard } from '../components';
 
 function Cart() {
+  const { cart } = useCartContext();
+  console.log('cart', cart);
   return (
-    <div>Cart</div>
+    <main className="flex flex-row gap-4 m-[80px] ">
+      <section>
+        {cart.map((product) => (
+          <CartItemCard product={product} />
+        ))}
+      </section>
+      <section>
+        <CartTotalCard cart={cart} />
+      </section>
+    </main>
   );
 }
 
