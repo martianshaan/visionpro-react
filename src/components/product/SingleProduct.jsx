f3d8857c6424cda0d346b6e3a58c550e0fa26a62
martianshaan <shantanu.kale@viit.ac.in>
2023-08-23 12:56:07 +0530

    Add: cartcontext and usecartcontext

--- src/components/product/SingleProduct.jsx
+++ src/components/product/SingleProduct.jsx
@@ -4,16 +4,18 @@
 import React from 'react';
 import { Star } from '@phosphor-icons/react';
 import { Heart } from 'lucide-react';
-// import { useProductContext } from '../../contexts';
+import { useCartContext } from '../../contexts';
 
 function SingleProduct({ product }) {
   const {
-    id, image, name, rating, price, brand,
+    id, image, name, rating, price, brand, newPrice,
   } = product;
-  console.log(id, image, name, rating, price, brand);
+  // console.log('newPrice', newPrice);
 
   // const { addProductToCart } = useProductContext();
 
+  const { addToCart, cart } = useCartContext();
+  console.log(cart);
   return (
     <section
       className="flex flex-col xs:flex-row sm:flex-col  bg-white/[0.5] rounded-lg shadow-md border-2 border-black/[0.05] overflow-hidden
@@ -37,7 +39,7 @@ function SingleProduct({ product }) {
             <section className="flex flex-col">
               <strong className="text-xl font-medium">{name}</strong>
               <span className="flex items-center gap-1">
-                <span>{product.rating}</span>
+                <span>{rating}</span>
 
                 <Star className=" text-yellow-400 mb-1" />
                 <span className="text-xs text-gray-400">Rating</span>
@@ -49,11 +51,11 @@ function SingleProduct({ product }) {
                 <span className="text-amber-600">
                   ₹
                   {' '}
-                  {product.newPrice}
+                  {newPrice}
                 </span>
               </p>
               <span className="text-sm text-gray-600 line-through">
-                {product.price}
+                {price}
               </span>
             </aside>
           </figcaption>
@@ -62,6 +64,7 @@ function SingleProduct({ product }) {
         <div className="w-full pt-2 border-t flex justify-between items-center">
           <button
             className="border border-[--primary-text-color]  py-1.5 text-sm  rounded-full px-6 hover:bg-[--primary-text-color] hover:text-white transition hover:shadow-md disabled:cursor-not-allowed"
+            onClick={() => addToCart(id, image, name, newPrice, brand)}
           >
             Add to Bag
           </button>
--- src/contexts/index.js
+++ src/contexts/index.js
@@ -3,6 +3,7 @@ import { useContext } from 'react';
 import { AuthContext } from './authContext/AuthContext';
 import { ProductContext } from './productContext/ProductContext';
 import { categoriesContext } from './categoriesContext/CategoriesContext';
+import { CartContext } from './cartContext/CartContext';
 
 export { default as AuthContextProvider } from './authContext/AuthContext';
 
@@ -10,8 +11,12 @@ export { default as ProductContextProvider } from './productContext/ProductConte
 
 export { default as CategoriesContextProvider } from './categoriesContext/CategoriesContext';
 
+export { default as CartContextProvider } from './cartContext/CartContext';
+
 export const useAuthContext = () => useContext(AuthContext);
 
 export const useProductContext = () => useContext(ProductContext);
 
 export const useCategoriesContext = () => useContext(categoriesContext);
+
+export const useCartContext = () => useContext(CartContext);
