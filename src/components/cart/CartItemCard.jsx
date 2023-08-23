6ce41873a5cf51ad9273b8469a9562d5a9fe56c6
martianshaan <shantanu.kale@viit.ac.in>
2023-08-23 17:21:59 +0530

    Add: onClick for remove product

--- src/components/cart/CartItemCard.jsx
+++ src/components/cart/CartItemCard.jsx
@@ -3,8 +3,10 @@
 import React from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 import { BsBookmarkHeart } from 'react-icons/bs';
+import { useCartContext } from '../../contexts';
 
 function CartItemCard({ product }) {
+  const { removeProductsFromCart } = useCartContext();
   return (
     <div className="m-auto flex flex-col gap-2  p-4 rounded-sm shadow-sm bg-white/[0.6] mb-2 max-w-xl">
       <div className="flex  items-center flex-wrap gap-2 w-full">
@@ -28,9 +30,10 @@ function CartItemCard({ product }) {
                 </button>
               </div>
               <div className="flex gap-3 ">
-                <button className="btn-rounded-secondary  text-sm mt-2 max-w-xs">
+                <button className="btn-rounded-secondary text-sm mt-2 max-w-xs" onClick={() => removeProductsFromCart(product.id)}>
                   Remove from Bag
                 </button>
+
                 <button>
                   <BsBookmarkHeart className="text-xl hover:text-rose-600 transition mt-1" />
                 </button>
