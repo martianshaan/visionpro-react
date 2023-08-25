b26dd171819d6b549b57c81d33552e9103667043
martianshaan <shantanu.kale@viit.ac.in>
2023-08-25 14:53:40 +0530

    Add: continue shopping handle

--- src/components/cart/CartTotalCard.jsx
+++ src/components/cart/CartTotalCard.jsx
@@ -1,27 +1,39 @@
 /* eslint-disable max-len */
 /* eslint-disable react/prop-types */
 import React from 'react';
+import { useNavigate } from 'react-router';
 import PriceCard from './PriceCard';
 
 function CartTotalCard({ cart }) {
+  const navigate = useNavigate();
   return (
-    <section className="md:col-span-1 py-7 px-7 rounded-md shadow-sm bg-white/[0.7] flex flex-col gap-6 w-full h-min">
+    <main className="md:col-span-1 py-4 px-7 rounded-md shadow-sm bg-white/[0.7] flex flex-col gap-6 w-full h-min">
       {cart.map((product) => (
         <PriceCard key={product.id} product={product} />
       ))}
 
       <hr />
-      <div className="flex justify-between items-center">
+      <section className="flex justify-between items-center">
         <p className=" text-gray-600">Total</p>
         <p className="text-2xl">₹ 90000</p>
-      </div>
+      </section>
 
-      <div className="w-full py-2   flex flex-col gap-4 items-center justify-center">
-        <button type="button" className="border bg-gray-900 text-white py-1.5  px-6  transition hover:bg-white hover:border-gray-800 hover:text-gray-800 rounded-full flex items-center gap-2 md:text-sm lg:text-base">
+      <section className="w-full mt-1 flex flex-col gap-3 items-center justify-center">
+        <button type="button" className="border w-full bg-gray-900 font-medium text-white py-1.5  px-6  transition hover:bg-white hover:border-gray-800 hover:text-gray-800 rounded-full shadow-sm flex items-center gap-2 md:text-sm lg:text-base">
           Proceed to Checkout
         </button>
-      </div>
-    </section>
+        <section className="flex justify-center text-center text-sm text-gray-500">
+          <p>
+            or
+            {' '}
+            <button type="button" className="font-medium text-black hover:text-amber-600" onClick={() => navigate('/glasses')}>
+              Continue Shopping
+              <span aria-hidden="true"> &rarr;</span>
+            </button>
+          </p>
+        </section>
+      </section>
+    </main>
   );
 }
 
