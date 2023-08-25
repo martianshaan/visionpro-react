b27660fc12e7ad7aedd84130dc90abbbf963d407
martianshaan <shantanu.kale@viit.ac.in>
2023-08-25 09:02:34 +0530

    Add: layout , clear cart,remove item,

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,19 +4,44 @@ import { useCartContext } from '../contexts';
 import { CartItemCard, CartTotalCard } from '../components';
 
 function Cart() {
-  const { cart } = useCartContext();
-  console.log('cart', cart);
+  const { cart, clearCartHandler } = useCartContext();
   return (
-    <main className="flex flex-row gap-4 m-[80px] ">
-      <section>
-        {cart.map((product) => (
-          <CartItemCard product={product} />
-        ))}
-      </section>
-      <section>
-        <CartTotalCard cart={cart} />
-      </section>
+    <main>
+      {cart.length === 0 ? (<h1 className="flex flex-row  mt-[80px] items-center justify-center">No Item in the cart</h1>)
+        : (
+          <main className="flex flex-row gap-4 mt-[80px] items-start align-middle justify-center">
+            <section className="flex flex-col gap-2">
+              <section className="flex flex-row justify-between gap-2">
+                <h1 className="text-xl text-custom-blue">
+                  My Cart (
+                  {cart.length}
+                  )
+                </h1>
+                <button
+                  type="button"
+                  className="border mr-2
+                 bg-gray-900 text-white py-1.5  px-6  transition
+                  hover:bg-white hover:border-gray-800 hover:text-gray-800
+                  rounded-full flex items-center gap-2 md:text-sm lg:text-base"
+                  onClick={() => clearCartHandler()}
+                >
+                  Clear Cart
+                </button>
+              </section>
+              <section className="flex flex-col  justify-center gap-3">
+                {cart.map((product) => (
+                  <CartItemCard product={product} />
+                ))}
+              </section>
+            </section>
+            <section className="flex flex-col gap-2">
+              <h1 className="text-xl text-custom-blue">Bill Details</h1>
+              <CartTotalCard cart={cart} />
+            </section>
+          </main>
+        )}
     </main>
+
   );
 }
 
