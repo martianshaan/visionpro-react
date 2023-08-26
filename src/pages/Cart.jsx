a2037e60c560839d41ec58a90d565b7b72d862c9
martianshaan <shantanu.kale@viit.ac.in>
2023-08-26 19:32:15 +0530

    update: Handler cart length zero and navigate

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,13 +1,26 @@
 /* eslint-disable no-console */
 import React from 'react';
+import { useNavigate } from 'react-router';
 import { useCartContext } from '../contexts';
 import { CartItemCard, CartTotalCard } from '../components';
 
 function Cart() {
   const { cart, clearCartHandler } = useCartContext();
+  const navigate = useNavigate();
   return (
     <main>
-      {cart.length === 0 ? (<h1 className="flex flex-row  mt-[80px] items-center justify-center">No Item in the cart</h1>)
+      {cart.length === 0 ? (
+        <section className="flex flex-col h-screen justify-center items-center gap-4 ">
+          <h1 className="font-medium text-lg">No Item in the cart</h1>
+          <button
+            type="button"
+            className="border bg-gray-900 font-medium text-white py-1.5  px-6  transition hover:bg-white hover:border-gray-800 hover:text-gray-800 rounded-full shadow-sm flex items-center gap-2 md:text-sm lg:text-base"
+            onClick={() => navigate('/glasses')}
+          >
+            Continue Shopping
+          </button>
+        </section>
+      )
         : (
           <main className="flex flex-row gap-4 mt-[80px] items-start align-middle justify-center">
             <section className="flex flex-col gap-2">
@@ -20,8 +33,8 @@ function Cart() {
                 <button
                   type="button"
                   className="border mr-2
-                 bg-gray-900 text-white py-1.5  px-6  transition
-                  hover:bg-white hover:border-gray-800 hover:text-gray-800
+                 bg-red-500 text-white py-1.5  px-6  transition shadow-sm
+                  hover:bg-red-600  hover:text-white-900
                   rounded-full flex items-center gap-2 md:text-sm lg:text-base"
                   onClick={() => clearCartHandler()}
                 >
