7baa4d8c4e814f3810cea849335cf6c8353e05b6
martianshaan <shantanu.kale@viit.ac.in>
2023-08-27 10:07:03 +0530

    Update:add total amount. payable and shipping fee

--- src/components/cart/CartTotalCard.jsx
+++ src/components/cart/CartTotalCard.jsx
@@ -8,26 +8,44 @@ import { useCartContext } from '../../contexts';
 
 function CartTotalCard({ cart }) {
   const navigate = useNavigate();
-  const { total_amount } = useCartContext();
+  const { totalAmount, shippingFee } = useCartContext();
   return (
-    <main className="md:col-span-1 py-4 px-7 rounded-md shadow-sm bg-white/[0.7] flex flex-col gap-6 w-full h-min">
+    <main className="py-4 px-5 border rounded-lg shadow-sm bg-white/[0.7] flex flex-col gap-2 justify-between w-full h-min md:col-span-1">
       {cart.map((product) => (
         <PriceCard key={product.id} product={product} />
       ))}
-
-      <hr />
+      <hr className="w-full border border-dashed border-purple-haze" />
       <section className="flex justify-between items-center">
-        <p className=" text-gray-600">Total</p>
+        <p className=" text-gray-600">Total Price</p>
+        <p className="text-base">
+          ₹
+          {' '}
+          {totalAmount}
+        </p>
+      </section>
+      <section className="flex justify-between items-center">
+        <p className=" text-gray-600">Shipping Fee</p>
+        <p className=" text-base ">
+          ₹
+          {' '}
+          {shippingFee}
+        </p>
+      </section>
+      <hr className="w-full border border-dashed border-purple-haze" />
+      <section className="flex justify-between items-center">
+        <p className=" text-gray-800 font-bold">Total Payable</p>
         <p className="text-2xl">
           ₹
           {' '}
-          {total_amount}
+          {shippingFee + totalAmount}
         </p>
       </section>
 
       <section className="w-full mt-1 flex flex-col gap-3 items-center justify-center">
-        <button type="button" className="border w-full bg-gray-900 font-medium text-white py-1.5  px-6  transition hover:bg-white hover:border-gray-800 hover:text-gray-800 rounded-full shadow-sm flex items-center gap-2 md:text-sm lg:text-base">
+        <button type="button" className="border w-full bg-gray-900 font-medium text-white py-1.5  px-6  transition hover:bg-white hover:border-gray-800 hover:text-gray-800 rounded-full shadow-sm flex items-center justify-center gap-2 md:text-sm lg:text-base">
           Proceed to Checkout
+          {' '}
+          {' >'}
         </button>
         <section className="flex justify-center text-center text-sm text-gray-500">
           <p>
