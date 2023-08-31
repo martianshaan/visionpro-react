dfb05e6566efb17b4f4df8cc3c61a09e348e6544
martianshaan <shantanu.kale@viit.ac.in>
2023-08-31 19:31:31 +0530

    Add: add to wish handler

--- src/contexts/WishlistContext/WishlistContext.jsx
+++ src/contexts/WishlistContext/WishlistContext.jsx
@@ -1,11 +1,35 @@
-import React, { createContext } from 'react'
+import React, { createContext, useReducer } from 'react'
+import { cartReducer } from '../../reducers/cartReducer';
+import { initialState } from '../../reducers/productsReducers';
+import { useAuthContext } from '../contextIndex';
+import { toast } from 'react-hot-toast';
 
 export const WishlistContext = createContext(); 
 
+const initialState={
+  wishlist:[]
+}
+
 const WishlistContextProvider = ({children}) => {
+
+  const {user}= useAuthContext();
+
+  const [state,dispatch]= useReducer(cartReducer,initialState)
+
+  const addToWishlistHandler=(product)=>{
+    if(!user===null){
+      dispatch({
+        type:'ADD_TO_WISHLIST',
+        dispatch: {...product,qty:1}
+      })
+      toast.success('Succesfull Product Added to Wishlist')
+    }else {
+      toast.error('Please Login to continue')
+    }
+  }
     
   return (
-    <WishlistContext.Provider>
+    <WishlistContext.Provider value={{...state,addToWishlistHandler}}>
         {children}
     </WishlistContext.Provider>
   )
