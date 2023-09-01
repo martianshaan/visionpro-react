639cbf28f8d1c7bd0e338007636eaea9677d3ba1
martianshaan <shantanu.kale@viit.ac.in>
2023-09-01 17:41:13 +0530

    Update: fixed errors in disptach

--- src/contexts/WishlistContext/WishlistContext.jsx
+++ src/contexts/WishlistContext/WishlistContext.jsx
@@ -1,8 +1,7 @@
-import React, { createContext, useReducer } from 'react'
-import { cartReducer } from '../../reducers/cartReducer';
-import { initialState } from '../../reducers/productsReducers';
+import React, { createContext, useReducer,useMemo } from 'react'
 import { useAuthContext } from '../contextIndex';
 import { toast } from 'react-hot-toast';
+import { WishlistReducer } from '../../reducers/wishlistReducer';
 
 export const WishlistContext = createContext(); 
 
@@ -12,24 +11,33 @@ const initialState={
 
 const WishlistContextProvider = ({children}) => {
 
-  const {user}= useAuthContext();
+  const { user }= useAuthContext();
 
-  const [state,dispatch]= useReducer(cartReducer,initialState)
+  const [state,dispatch]= useReducer(WishlistReducer,initialState)
 
-  const addToWishlistHandler=(product)=>{
-    if(!user===null){
-      dispatch({
-        type:'ADD_TO_WISHLIST',
-        dispatch: {...product,qty:1}
-      })
-      toast.success('Succesfull Product Added to Wishlist')
-    }else {
-      toast.error('Please Login to continue')
+ const addToWishlistHandler = (product) => {
+    if (user) {
+        dispatch({
+            type: 'ADD_TO_WISHLIST',
+            payload: { ...product, qty: 1 } 
+        });
+        toast.success('Successfully added the product to Wishlist');
+    } else {
+        toast.error('Please log in to continue');
     }
-  }
+};
+
+  const isInWishlist = useMemo(() => (productId) => state.wishlist.find(
+    (item) => item.id === productId
+  ), [state.wishlist]);
     
   return (
-    <WishlistContext.Provider value={{...state,addToWishlistHandler}}>
+    <WishlistContext.Provider 
+    value={{
+      ...state,
+    wishlist:state.wishlist,
+    isInWishlist,
+    addToWishlistHandler}}>
         {children}
     </WishlistContext.Provider>
   )
