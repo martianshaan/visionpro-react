38410143809d0606d11b62f5a97518162d203bca
martianshaan <shantanu.kale@viit.ac.in>
2023-09-02 00:29:35 +0530

    Add:  button for clear cart and add num wish prod

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -3,11 +3,26 @@ import { useWishlistContext } from '../contexts/contextIndex'
 import WishlistItemCard from '../components/wishlist/WishlistItemCard'
 
 const Wishlist = () => {
-    const {wishlist}= useWishlistContext()
+    const { wishlist , clearWishlistHandler}= useWishlistContext()
   return (
     <main className='flex flex-col rounded-md border-gray-700 mt-[72px]  justify-center  items-center'>
-          <h1 className='text-2xl mb-3'>My Wishlist</h1>
-      <section className='grid grid-cols-2 m-auto  md:grid-cols-2 lg:grid-cols-4 sm:grid-cols-2 gap-5'>
+          <section className='flex gap-20 w-11/12 justify-between px-2 my-2 items-middle '>
+            <section className=' flex justify-start gap-3  items-end'>
+            <h1 className='text-xl'>My Wishlist</h1>
+            <span className='text-gray-400 text-xl'>{wishlist.length} products</span>
+            </section>
+            <button
+                  type="button"
+                  className="border mr-2
+                 bg-red-500 text-white py-1.5  px-6  transition shadow-sm
+                  hover:bg-red-600  hover:text-white-900
+                  rounded-full flex items-center gap-2 md:text-sm lg:text-base"
+                  onClick={() => clearWishlistHandler()}
+                >
+                  Clear Wishlist
+              </button>
+          </section>
+      <section className='grid grid-cols-2 m-auto  justify-items-center md:grid-cols-2 lg:grid-cols-4 sm:grid-cols-2 gap-5 mt-2 md:px-2'>
              {wishlist.map((product)=>(
                 <WishlistItemCard product={product} />
              ))}
