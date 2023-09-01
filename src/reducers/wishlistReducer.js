86cfcf6ad65ec1d96d2c05ed907ca3dc18fae7d8
martianshaan <shantanu.kale@viit.ac.in>
2023-09-01 17:41:50 +0530

    Update: fixed errors

--- src/reducers/wishlistReducer.js
+++ src/reducers/wishlistReducer.js
@@ -1,16 +1,19 @@
-export const WishlistReducer=(state,action)=>{
-    const {type,payload}=action
-    switch(type){
-        case 'ADD_TO_WISHLIST': 
-        const existingWishlistItemIndex=state.wishlist.map((item)=>item.id===product.id)
-        if(existingWishlistItemIndex !==-1){
-            const updatedWishlist= state.wishlist.map((item,index)=(
-                index===existingWishlistItemIndex ? {...item,qty:item.qty+1}:item
-            ))
-            return {...state,wishlist:updatedWishlist}
-        }else{
-            return {...state,wishlist:[...wishlist,payload]}
-        }
-       
+export const WishlistReducer = (state, action) => {
+    const { type, payload } = action;
+    switch (type) {
+        case 'ADD_TO_WISHLIST':
+            const existingWishlistItemIndex = state.wishlist.findIndex(item => item.id === payload.id);
+
+            if (existingWishlistItemIndex !== -1) {
+                const updatedWishlist = state.wishlist.map((item, index) => (
+                    index === existingWishlistItemIndex ? { ...item, qty: item.qty + 1 } : item
+                ));
+                return { ...state, wishlist: updatedWishlist };
+            } else {
+                return { ...state, wishlist: [...state.wishlist, payload] };
+            }
+
+        default:
+            return state;
     }
-}
+};
