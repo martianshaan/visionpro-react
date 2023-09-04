e1fd11981539ef28ee05b5b82d3689dea0f17812
martianshaan <shantanu.kale@viit.ac.in>
2023-09-04 07:55:57 +0530

    update: responsive ness

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -5,8 +5,8 @@ import WishlistItemCard from '../components/wishlist/WishlistItemCard'
 const Wishlist = () => {
     const { wishlist , clearWishlistHandler}= useWishlistContext()
   return (
-    <main className='flex flex-col rounded-md border-gray-700 mt-[72px]  justify-center  items-center'>
-          <section className='flex gap-20 w-11/12 justify-between px-2 my-2 items-middle '>
+    <main className='mt-[72px]'>
+          <section className='flex gap-17 w-11/12 justify-between px-2 my-2 mx-12  items-end  '>
             <section className=' flex justify-start gap-3  items-end'>
             <h1 className='text-xl'>My Wishlist</h1>
             <span className='text-gray-400 text-xl'>{wishlist.length} products</span>
@@ -22,7 +22,7 @@ const Wishlist = () => {
                   Clear Wishlist
               </button>
           </section>
-      <section className='grid grid-cols-2 m-auto  justify-items-center md:grid-cols-2 lg:grid-cols-4 sm:grid-cols-2 gap-5 mt-2 md:px-2'>
+      <section className='grid grid-cols-2 gap-6 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4  m-auto mx-12'>
              {wishlist.map((product)=>(
                 <WishlistItemCard product={product} />
              ))}
