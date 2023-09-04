0d4f4766d536ec2b5d630c8e3f32055352ec8ecf
martianshaan <shantanu.kale@viit.ac.in>
2023-09-04 07:10:08 +0530

    Update: prevent parent firing for close icon

--- src/components/wishlist/WishlistItemCard.jsx
+++ src/components/wishlist/WishlistItemCard.jsx
@@ -2,29 +2,46 @@ import React from 'react';
 import { X } from '@phosphor-icons/react';
 import { useCartContext, useWishlistContext } from '../../contexts/contextIndex';
 import { toast } from 'react-hot-toast';
+import { useNavigate } from 'react-router';
 
 
 
 const WishlistItemCard = ({ product }) => {
     const { id, image, name, newPrice, price } = product
 
-    const { removeProductFromWishlistHandler , isInWishlist } = useWishlistContext();
+    const { removeProductFromWishlistHandler, isInWishlist } = useWishlistContext();
 
-    const { addToCart } = useCartContext()
+    const { addToCart } = useCartContext();
 
-    const productIsInWishlist= isInWishlist(id)
-    const moveToBagHandler=()=>{
-        if(productIsInWishlist){
+    const navigate = useNavigate()
+
+    const productIsInWishlist = isInWishlist(id);
+
+
+    const handleRemoveWishlistProduct=(event)=>{
+        //To stop parent evnts from firing use this 
+        event.stopPropagation();
+
+        removeProductFromWishlistHandler(id)
+    }
+
+    const moveToBagHandler = () => {
+        if (productIsInWishlist) {
             addToCart(product);
             removeProductFromWishlistHandler(id);
-        }else{
+        } else {
             toast.error('Error ')
         }
     }
+
     return (
-        <main className='flex flex-col  bg-white border  rounded-xl border-gray-200 gap-2 justify-center align-middle items-center'>
+        <main className='flex flex-col xs:flex-row sm:flex-col bg-white/[0.75] rounded-lg  shadow-sm shadow-amber-50 border-1   border-gray-200 gap-2 
+        justify-center align-middle items-center overflow-hidden cursor-pointer transition-shadow   hover:scale-[1.05] hover:shadow-md'>
             <figure
-                className=" relative flex items-center justify-center border  rounded-t-xl p-6 xs:p-5 sm:p-5 bg-black/[0.075]  xs:w-1/2 w-full sm:w-full"
+                className=" relative flex items-center justify-center p-10 xs:p-5 sm:p-10 bg-black/[0.075]  xs:w-1/2 w-full sm:w-full"
+                onClick={() => {
+                    navigate(`/glasses/${id}`);
+                }}
             >
                 <img
                     src={image}
@@ -34,7 +51,7 @@ const WishlistItemCard = ({ product }) => {
                 <button
                     type='button'
                     className='absolute top-2 right-2 p-1 rounded-full bg-white bg-opacity-70 border border-gray-300'
-                    onClick={() => removeProductFromWishlistHandler(id)}
+                    onClick={handleRemoveWishlistProduct}
                 >
                     <X size={15} weight="thin" />
                 </button>
