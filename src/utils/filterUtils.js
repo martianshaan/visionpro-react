37eaea3520aa94c64811c3cc14a72af4abfedd6f
martianshaan <shantanu.kale@viit.ac.in>
2023-09-05 07:57:34 +0530

    Add: filter and sort functions

---
+++ src/utils/filterUtils.js
@@ -0,0 +1,48 @@
+export const filterByGender=(selectedGender,data)=>{
+    const lowerSelectedGender = selectedGender.toLowerCase();
+    if(!selectedGender || selectedGender.toLowerCase() === 'all'){
+        return data
+    } else {
+      return data.filter(
+        //used destrusruting to  extracting the gender property from each object in the data array.
+       ({gender})=> gender.toLowerCase() === lowerSelectedGender
+      )
+    }
+}
+
+
+export const filterByPriceRange=(selectedPriceRange,data)=>{
+    return selectedPriceRange ? 
+    data.filter(({newPrice})=> newPrice <= selectedPriceRange) : data;
+};
+
+//selectedCategories is an array
+export const filterByCheckbox =(selectedCategories,data)=>{
+    return selectedCategories.length >0 ? data.filter((
+        { category })=>selectedCategories.includes(category.toLowerCase())):data;
+};
+
+//added if else condtn
+export const filterByRating=(selectedRating,data)=>{
+    return selectedRating ?  data.filter(({rating}) => rating >= selectedRating):data
+}
+
+export const sortByPrice=(type,data)=>{
+    const sortedData = [...data];
+    if(type === 'LOW_TO_HIGH'){
+        return sortedData.sort((a,b)=> a.newPrice -b.newPrice);
+    }else if(type === 'HIGH_TO_LOW'){
+        return sortedData.sort((a,b)=>b.newPrice-a.newPrice);
+    }
+    return data
+}
+
+export const filterBySearch = (searchText, data) => {
+    const lowerSearchText = searchText.toLowerCase();
+  
+    if (!searchText) {
+      return data;
+    }
+  
+    return data.filter(({ name }) => name.toLowerCase().includes(lowerSearchText));
+  };
