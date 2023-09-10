067c95d9f9d5ef50ebbe784df97adddd89582714
martianshaan <shantanu.kale@viit.ac.in>
2023-09-10 18:12:18 +0530

    update: import categorylist

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ export { default as Navbar } from './navbar/Navbar';
 export { default as Banner } from './banner/Banner';
 export { default as Trending } from './trending/TrendingList';
 export { default as SingleProduct } from './product/SingleProduct';
+export { default as CategoryList } from './category/CategoryList';
 export { default as Filters } from './filters/Filters';
 export { default as Logo } from './navbar/Logo';
 export { default as CartItemCard } from './cart/CartItemCard';
