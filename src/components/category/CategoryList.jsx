001ec537b56074ed66f95af5ea9d3a79ec0b8716
martianshaan <shantanu.kale@viit.ac.in>
2023-09-11 20:00:12 +0530

    update: grid error removed

--- src/components/category/CategoryList.jsx
+++ src/components/category/CategoryList.jsx
@@ -8,16 +8,14 @@ const CategoryList = () => {
     console.log(categories);
     return (
         <main>
-            <h1 className=" break-words underline text-black mt-5 mb-2 text-3xl md:text-4xl   text-center ">
+            <h1 className="mt-5 mb-2 text-3xl text-center text-black underline break-words md:text-4xl">
                 Categories
             </h1>
-            <section className="grid grid-cols-1 md:grid-cols-3 gap-12 p-4 w-11/12 m-auto">
+            <section className="grid grid-cols-1 md:grid-cols-3 gap-12 p-4  m-auto ">
                 {categories.length > 0 ? (
-                   <section> 
-                   {categories.map((category) => (
+                   categories.map((category) => (
                         <CategoryCard category={category} key={category._id} />
-                    ))} 
-                </section>
+                    ))
                 ):(
                 <article>
                     No categories to show !
