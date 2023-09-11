0f48a359f08f31a3b416c76a2e43e574d2a58565
martianshaan <shantanu.kale@viit.ac.in>
2023-09-11 20:11:45 +0530

    Update: entire banner  secion updated

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,25 +1,32 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import { useNavigate } from 'react-router';
 import { BsArrowDownRightCircle } from 'react-icons/bs';
-import bannerImg from '../../assets/bannerImg.png';
+import HomePhoto from '../../assets/HomePhoto.png'
+import HomePhotoBroadLong from '../../assets/HomePhotoBroadLong.png'
+import HomePhotoBroadLong2 from '../../assets/HomePhotoBroadLong2.png'
+import HomePhotoBroadMobile from '../../assets/HomePhotoBroadMobile.png'
+import BroadHome from '../../assets/BroadHome.png'
+
 
 function Banner({ catRef }) {
+  const navigate = useNavigate();
   return (
-    <main className="flex justify-between items-center mt-1">
-      <section>
-        <h1 className="text-6xl sm:text-7xl lg:text-8l font-semibold py-3 w-full">
-          Glasses and Lens
+    <main className="relative flex items-center justify-between bg-[--theme-color] ">
+      <section className='absolute text-center inset-0 flex flex-col justify-top items-start p-4 mt-[70px] md:mt-[50px]'>
+        <h1 className="w-full py-3 text-3xl font-semibold sm:text-7xl lg:text-8l ">
+          SUNBUN
         </h1>
-        <p className=" py-3 text-md text-grey-300">
+        <h1 className="w-full py-3 text-xl  sm:text-3xl ">
+          Let's Elevate Your Frame Game
+        </h1>
+        <p className=" md:py-3 flex justify-center items-start text-[7px]  text-white font-semibold md:text-3xl text-grey-300 absolute top-[255px] md:top-[550px] left-[127px] md:left-[350px]">
           Buy the best high-quality sunglasses from us.
           <br />
           More than 100 types of assortment.
         </p>
-        <section className="flex items-center">
-          <button type="submit" className="btn-secondary text-sm md:text-base" onClick={() => {}}>
-            Start Shopping
-          </button>
-          <button type="button" className="p-3 flex items-center" onClick={() => { catRef.current.scrollIntoView({ behavior: 'smooth' }); }}>
+        <section className="flex items-center absolute top-[255px] md:top-[500px] left-[147px] md:left-[560px] text-white ">
+          <button type="button" className="flex items-center p-3" onClick={() => { catRef.current.scrollIntoView({ behavior: 'smooth' }); }}>
             <span className="mx-2 texts-sm md:text-base">
               Explore More
             </span>
@@ -27,8 +34,20 @@ function Banner({ catRef }) {
           </button>
         </section>
       </section>
-      <section>
-        <img src={bannerImg} alt="bannerImg" className="w-23 h-full" />
+      <section className="absolute top-[255px] md:top-[490px] left-[147px] md:left-[550px] ">
+        <button type="submit" className="px-1 py-1 font-medium text-white bg-orange-800 rounded-md bg-gradient-to-b from-orange-400 md:rounded-2xl md:px-7 md:py-3 md:font-extrabold hover:bg-orange-600 texst-xs md:text-lg " 
+        onClick={() => navigate('/glasses')}>
+          Open Catalog
+        </button>
+      </section>
+      {/* <section className='left-0 right-0 flex items-center justify-center text-center -mt-[152px]'>
+        <img src={HomePhoto} alt="bannerImg" className=" object-cover h-sreen  w-screen  " />
+      </section> */}
+      <section className='left-0 right-0 items-center justify-center hidden text-center sm:block '>
+        <img src={HomePhotoBroadLong2} alt="bannerImg" className=" object-cover h-sreen  w-screen " />
+      </section>
+      <section className='left-0 right-0 flex items-center justify-center text-center mt-[90px]'>
+        <img src={HomePhotoBroadMobile} alt="bannerImg" className="object-cover w-screen h-sreen sm:hidden" />
       </section>
     </main>
   );
