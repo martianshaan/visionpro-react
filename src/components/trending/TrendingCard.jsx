16af0a02f47262082cb0a1e9be9d39f5ce80457c
martianshaan <shantanu.kale@viit.ac.in>
2023-09-11 19:59:12 +0530

    update:navigationa nd add to cart functionality

--- src/components/trending/TrendingCard.jsx
+++ src/components/trending/TrendingCard.jsx
@@ -2,36 +2,62 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { AiOutlinePlus } from 'react-icons/ai';
+import { useNavigate } from 'react-router';
+import { useAuthContext, useCartContext,  } from '../../contexts/contextIndex';
+import {toast} from 'react-hot-toast';
 
 function TrendingCard({ product }) {
+  const { user } = useAuthContext();
+  const {addToCart, isInCart }= useCartContext();
+  const navigate = useNavigate();
+  
+  const productIsInCart = isInCart(product.id);
   return (
-    <section className="flex flex-col p-4 rounded-xl bg-black/[.06] cursor-pointer gap-3">
-      <figure className="flex justify-center items-center w-full h-full ap-3 xs:flex-wrap xs:justify-center sm:flex-nowrap sm:justify-between">
+    <section className="flex flex-col gap-2 px-4 py-1 rounded-xl bg-black/[.06] cursor-pointer "
+    >
+
+      <figure className="flex justify-center items-start w-full h-full py-1">
         <img
-          src={product.img}
+          src={product.image}
           alt={product.name}
-          className=" w-1/2 h-100 py-2  xs:w-2/3 xs:h-16 sm:w-2/3 sm:h-100  object-cove"
+          className="w-32 h-20 md:w-2/3 md:h-30 object-cover py-2"
+          onClick={() => navigate('/glasses/' + product.id)}
         />
       </figure>
-      <div className="flex justify-between">
-        <div>
-          <h1 className="text-2xl font-bol">{product.name}</h1>
-          <p className="text-gray-600 text-sm">{product.category}</p>
-        </div>
-        <div className="flex items-start">
-          <h1 className="mx-1 text-2xl font-sans font-bold">
+      <section className="flex items-center justify-between gap-3  xs:flex-wrap xs:justify-center sm:flex-nowrap sm:justify-between">
+        <article>
+          <h1 className="text-base font-bold sm:text-2xl">{product.name}</h1>
+          <p className="text-sm text-gray-600 ">{product.category}</p>
+        </article>
+        <article className="flex items-start">
+          <h1 className="mx-1 font-sans text-2xl font-bold">
             ₹
-            {product.price}
+            {product.newPrice}
           </h1>
           <button
             type="button"
-            className="p-3 custom-bg-gradient rounded-md
-          mx-2 px-2 py-2 shadow-sm  text-white bg-yellow-700 text-sm hover:bg-yellow-800 transition"
+            className="p-3 px-2 py-2 mx-2 text-sm text-white transition bg-orange-800 rounded-md shadow-sm bg-gradient-to-b from-orange-400 hover:bg-orange-900"
+            onClick={() => {
+              if (!user) {
+                navigate('/login', { state: { from: location.pathname } });
+                toast('Please login to continue shopping', {
+                  icon: '⚠️',
+                });
+              } else if (productIsInCart) {
+                toast('Please  Already added in Bag', {
+                  icon: '⚠️',
+                });
+              }else if (!productIsInCart) {
+                addToCart(product);
+              } else {
+                navigate('/cart');
+              }
+            }}
           >
-            <AiOutlinePlus className="text-white font-bold" />
+            <AiOutlinePlus className="text-sm font-bold text-white" />
           </button>
-        </div>
-      </div>
+        </article>
+      </section>
 
     </section>
   );
