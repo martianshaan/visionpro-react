88a5bcd779dc0c65257f5db1dca13885e48eb10f
martianshaan <shantanu.kale@viit.ac.in>
2023-09-11 19:59:38 +0530

    Update: make trending list dynamics

--- src/components/trending/TrendingList.jsx
+++ src/components/trending/TrendingList.jsx
@@ -1,51 +1,13 @@
 import React from 'react';
-import glassCategory1 from '../../assets/glassCategory1.png';
-import glassCategory2 from '../../assets/glassCategory2.png';
-import glassCategory3 from '../../assets/glassCategory3.png';
 import TrendingCard from './TrendingCard';
+import { trendingProducts } from '../../backend/db/trendingProducts';
 
-function TrendingList() {
-  const trendingProducts = [
-    {
-      id: 1,
-      name: 'Studio',
-      category: 'Vision',
-      price: '223',
-      img: glassCategory1,
-    },
-    {
-      id: 2,
-      name: 'Boost',
-      category: 'Sports',
-      price: '459',
-      img: glassCategory2,
-    },
-    {
-      id: 3,
-      name: 'Rim',
-      category: 'Sunglass',
-      price: '175',
-      img: glassCategory3,
-    },
-    {
-      id: 4,
-      name: 'Rim',
-      category: 'Sunglass',
-      price: '175',
-      img: glassCategory3,
-    },
-    {
-      id: 5,
-      name: 'Rim',
-      category: 'Sunglass',
-      price: '175',
-      img: glassCategory3,
-    },
-  ];
+ function TrendingList() {
+
   return (
-    <section className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3   lg:grid-cols-4 gap-4  py-4 mt-10">
-      <h1 className="text-4xl lg:text-5xl break-words flex items-center">
-        Trending Products
+    <section className="grid grid-cols-1 gap-4 py-4 mt-10 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mx-2">
+      <h1 className="flex items-center break-words text-12xl text text- lg:text-5xl">
+       Popular Products
       </h1>
       {trendingProducts.map((product) => (
         <TrendingCard key={product.id} product={product} />
