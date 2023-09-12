467e7087ddc543d6ef20bcd7fa84b3e39c2b4ed2
martianshaan <shantanu.kale@viit.ac.in>
2023-09-12 22:21:22 +0530

    update: add

--- src/components/trending/TrendingList.jsx
+++ src/components/trending/TrendingList.jsx
@@ -5,9 +5,9 @@ import { trendingProducts } from '../../backend/db/trendingProducts';
  function TrendingList() {
 
   return (
-    <section className="grid grid-cols-1 gap-4 py-4 mt-10 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mx-2">
-      <h1 className="flex items-center break-words text-12xl text text- lg:text-5xl">
-       Popular Products
+    <section className="grid grid-cols-1 gap-4 md:py-4 mt-6  md:mt-10 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mx-2 ">
+      <h1 className="flex items-center justify-center text-3xl text-center md:text-5xl">
+       Popular{' '} Products      
       </h1>
       {trendingProducts.map((product) => (
         <TrendingCard key={product.id} product={product} />
