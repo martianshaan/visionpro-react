b01def2df95c3e0b314a656eacef408d55e7392a
martianshaan <shantanu.kale@viit.ac.in>
2023-09-20 20:09:55 +0530

    update :color

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <section className="w-full bg-amber-100 text-center py-10 mt-1">
+    <section className="w-full bg-amber-200 text-center py-4 mt-1">
        Sunbun 🇮🇳 MADE IN INDIA
     </section>
   );
