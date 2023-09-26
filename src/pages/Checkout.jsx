93cca16ce142b83e83dfb18b9a2debd13a7c8f93
martianshaan <shantanu.kale@viit.ac.in>
2023-09-26 13:53:35 +0530

    update: redirect page  ?cart length is 0  to  home

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,13 +1,25 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react/prop-types */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Address from '../components/address/Address';
 import SummaryCard from '../components/Checkout/SummaryCard';
 import OrderConfirmModal from '../components/Checkout/OrderConfirmModal';
-
+import { useLocation, useNavigate } from 'react-router';
+import { useCartContext } from '../contexts/contextIndex';
 
 function Checkout() {
-  const [showModal, setShowModal] = useState(false)
+  const [showModal, setShowModal] = useState(false);
+  const { cart } = useCartContext();
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  console.log({ location });
+  useEffect(() => {
+    if (location?.state !== "cart" || !cart.length) {
+      navigate("/");
+    }
+  }, []);
   return (
     <main className="md:min-h-[80vh] flex justify-center items-center p-1 mt-[72px] ">
       <section className="grid md:grid-cols-2  rounded-lg w-full gap-6 ">
